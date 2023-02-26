80a9a855723188943649a83aeb5f380d6ae5e9b5
AlphaSheep <brendan@alphasheep.co.za>
2023-02-26 11:17:31 +0200

    Add a button to register competitors who have paid but aren't registered

--- src/components/guest-list/index.js
+++ src/components/guest-list/index.js
@@ -42,12 +42,12 @@ export default function GuestList({quicketEvent, wcaComp, stateContainer}) {
     <EventLinks wcaComp={wcaComp} quicketEvent={quicketEvent}/>
 
     {tickets.length && registrations.length ? 
-    <RegistrationTables tickets={tickets} registrations={registrations}/>
+    <RegistrationTables tickets={tickets} registrations={registrations} compId={wcaComp.id}/>
     : null}
   </div>
 }
 
-function RegistrationTables({ tickets, registrations }) {
+function RegistrationTables({ tickets, registrations, compId}) {
   const {pending, unregistered, unpaid, acceptedMissingPayment, acceptedPaid, deleted} = matchRegistrationsToPayments(tickets, registrations);
   return <div className='guest-list'>
     <h3>Pending</h3>
@@ -55,7 +55,7 @@ function RegistrationTables({ tickets, registrations }) {
     <h3>Awaiting payment</h3>
     <RegistrationTable data={unpaid} className="awaiting"/>
     <h3>Not yet registered</h3>
-    <RegistrationTable data={unregistered} className="unregistered"/>
+    <RegistrationTable data={unregistered} className="unregistered" showRegisterButton={true} compId={compId}/>
     <h3>Accepted but no payment matched</h3>
     <RegistrationTable data={acceptedMissingPayment} className="accepted-not-paid"/>
     <h3>Accepted</h3>
@@ -65,7 +65,7 @@ function RegistrationTables({ tickets, registrations }) {
   </div>
 }
 
-function RegistrationTable({data, className}) {
+function RegistrationTable({data, className, showRegisterButton, compId}) {
   return <div> 
       <table className={className}>
         <thead>
@@ -79,7 +79,7 @@ function RegistrationTable({data, className}) {
         <tbody>
           {data.map(row => <tr key={row.wcaid+row.name+row.email}>
             <td>{row.wcaid}</td>
-            <td>{row.name}</td>
+            <td>{showRegisterButton ? <RegisterButton data={row} compId={compId}/> : null} {row.name}</td>
             <td>{row.email}</td>
             <td className='status-column'><StatusTags data={row}/></td>
           </tr>)}
@@ -95,4 +95,15 @@ function StatusTags({data}) {
     {data.organizer ? <span className='status-tag'>Organiser</span> : null}
     {data.delegate ? <span className='status-tag'>Delegate</span> : null}
   </>
+}
+
+function RegisterButton({data, compId}) {
+  return <a href={getAddRegistrationLink(data, compId)} target='_blank' className='add-registration-button'>Add</a>
+}
+
+function getAddRegistrationLink(data, compId) {
+  const base = process.env.REACT_APP_WCA_URL;
+  const wcaId = data.wcaid ? data.wcaid : "";
+  const params = `registration_data[name]=${data.name}&registration_data[email]=${data.ticket_email}&registration_data[wca_id]=${wcaId}`;
+  return `${base}/competitions/${compId}/registrations/add?${params}`;
 }
